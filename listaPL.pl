c427e6e386684760122a9820f0321359a2660b7f
MateusKen <54600427+MateusKen@users.noreply.github.com>
2024-05-16 22:54:36 -0300

    Update listaPL.pl

--- listaPL.pl
+++ listaPL.pl
@@ -8,3 +8,15 @@ onde(E,[_|Xs],R) :- onde(E,Xs,R1), R is R1+1.
 ateh(_,[],[]).
 ateh(E,[E|_],[E]).
 ateh(E,[A|Xs],[A|Bs]) :- ateh(E,Xs,Bs).
+
+/*EXERCICIO 3*/
+apos(_,[],[]).
+apos(E,[E|Xs],Xs).
+apos(E,[A|Xs],R) :- apos(E,Xs,R).
+
+/*EXERCICIO 4*/
+npri(N, List) :- npri(N, 1, [], List).
+npri(N, Curr, Acc, List) :- Curr > N, reverse(Acc, List).
+npri(N, Curr, Acc, List) :-Curr =< N,NewCurr is Curr+1,npri(N, NewCurr, [Curr|Acc], List).
+
+/*EXERCICIO 5*/
